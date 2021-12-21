93fcc3a588a3f46b774119b7edc023b7988acbbc
999666s <ninesix666999@mail.ru>
2021-12-21 16:18:18 +0300

    Evalargs version

--- cdamage.py
+++ cdamage.py
@@ -11,16 +11,21 @@ def calculate_damage(lvl, attack, defense, base, stab, type_):
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("lvl", type=int, help="Lead pokemon lvl")
-    parser.add_argument("attack", type=int, help="Lead pokemon attack")
-    parser.add_argument("defense", type=int, help="Defensive pokemon defense")
-    parser.add_argument("base", type=int, help="Attack base")
-    parser.add_argument("stab", type=float, help="Stab multiplier")
-    parser.add_argument("typem", type=float, help="Type multiplier")
-    
-
+    parser.add_argument("lvl", type=str, help="Lead pokemon lvl")
+    parser.add_argument("attack", type=str, help="Lead pokemon attack")
+    parser.add_argument("defense", type=str, help="Defensive pokemon defense")
+    parser.add_argument("base", type=str, help="Attack base")
+    parser.add_argument("stab", type=str, help="Stab multiplier")
+    parser.add_argument("typem", type=str, help="Type multiplier")
     args = parser.parse_args()
-    min_damage, max_damage, crit, average = calculate_damage(args.lvl, args.attack, args.defense, args.base, args.stab, args.typem)
+
+    lvl = int(eval(args.lvl))
+    attack = int(eval(args.attack))
+    defense = int(eval(args.defense))
+    base = int(eval(args.base))
+    stab = int(eval(args.stab))
+    type_multiplier = int(eval(args.typem))
+    min_damage, max_damage, crit, average = calculate_damage(lvl, attack, defense, base, stab, type_multiplier)
     print(f"Minimal damage:\t  {min_damage}")
     print(f"Max damage:\t  {max_damage}")
     print(f"Average damage:\t  {average}")
